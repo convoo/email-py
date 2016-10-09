f355404614a3682e901ba7d23d13c6eb5ad443d5
Saad Elbeleidy <sbeleidy90@gmail.com>
2016-10-09 02:13:46 -0600

    add sendgrid

--- main.py
+++ main.py
@@ -1,11 +1,28 @@
+#!/usr/bin/python
+# coding: utf-8
 
 # [START app]
 import logging
 
 from flask import Flask, jsonify
 
+# settings.py
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+SENDGRID_KEY = os.environ.get("SENDGRID_KEY")
+
+
+# using SendGrid's Python Library - https://github.com/sendgrid/sendgrid-python
+import sendgrid
+from sendgrid.helpers.mail import *
 
 app = Flask(__name__)
+
 
 
 @app.route('/')
@@ -13,9 +30,22 @@ def hello():
     return jsonify({'message': "Hello!"})
 
 
+@app.route('/email')
+def email():
+    print(SENDGRID_KEY)
+    sg = sendgrid.SendGridAPIClient(apikey=SENDGRID_KEY)
+    from_email = Email("teamconvoo@gmail.com")
+    subject = "Hello World from the SendGrid Python Library!"
+    to_email = Email("teamconvoo@gmail.com")
+    content = Content("text/plain", "Hello, Email!")
+    mail = Mail(from_email, subject, to_email, content)
+    response = sg.client.mail.send.post(request_body=mail.get())
+    return jsonify({'message': "Email sent!"})
+
+
 @app.errorhandler(500)
 def server_error(e):
     # Log the error and stacktrace.
     logging.exception('An error occurred during a request.')
-    return 'An internal error occurred.', 500
+    return jsonify({'message':'An internal error occurred.','status': 500})
 # [END app]
