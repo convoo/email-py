d6661aa602e52f6552825356f1d19190e51c12aa
@bkawk <email.will.in.china@gmail.com>
2016-10-11 00:15:04 +0800

    template engine

--- main.py
+++ main.py
@@ -8,14 +8,14 @@
 import time, logging, configparser 
 import pyrebase
 import sendgrid
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 from sendgrid.helpers.mail import *
 
 # environment variables 
 config = configparser.ConfigParser()
 config.read('.env')
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='views', static_folder='public')
 
 # firebase config
 firebaseConfig = {
@@ -46,27 +46,45 @@ def prepareEmail(path, data):
     print(data['fromEmail'])
     db.child("email/sent").push(data)
     db.child("email/queue").child(path).remove()
-    # have we sent this guy the same email before?
-    sendEmail(data['fromEmail'], data['subject'], data['toEmail'], data['contentType'], data['content'])
-    
+    timeNow = int(time.time())
+    timeBefore = int(time.time()) - 84946 #24 hours ago
+    # Have we sent this gut an email before?
+    #sentToday = db.child("email/sent").order_by_child("time").start_at(timeNow).end_at(timeBefore).get()
+    #print(sentToday)
+    sendEmail(data['fromEmail'], data['subject'], data['toEmail'], data['contentType'], data['mailContent'])
 
 # send email 
-def sendEmail(fromEmail, subject, toEmail, contentType, Content):
+def sendEmail(fromEmail, subject, toEmail, contentType, mailContent):
     if fromEmail and subject and toEmail and contentType and Content:
         print('SENDING')
         # sg = sendgrid.SendGridAPIClient(apikey=config['SENDGRID']['KEY'])
-        # from_email = Email("teamconvoo@gmail.com")
-        # subject = "You are officially on the Beta List!"
-        # to_email = Email("people@gmail.com")
-        # content = Content("text/plain", "Hello, Email!")
+        # from_email = Email(fromEmail)
+        # to_email = Email(toEmail)
+        # content = Content(contentType, mailContent)
         # mail = Mail(from_email, subject, to_email, content)
         # response = sg.client.mail.send.post(request_body=mail.get())
 
 # place an email in the queue for testng 
-@app.route('/')
+@app.route('/test')
 def hello(): 
-    db.child("email/queue").push({"fromEmail":"teamconvoo@gmail.com","subject":"You are officially on the Beta List!", "toEmail":"email.will.in.china@gmail.com", "contentType":"text/plain", "content":"Woohoo!", "time": int(time.time())})
-    return jsonify({'message': "Hello!"})
+    db.child("email/queue").push({"fromEmail":"teamconvoo@gmail.com","subject":"You are officially on the Beta List!", "toEmail":"email.will.in.china@gmail.com", "contentType":"text/plain", "mailContent":"Woohoo!", "time": int(time.time())})
+    return jsonify({'message': "We sent a test email!"})
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+@app.route('/pay')
+def pay():
+    return render_template('pay.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
 
 # handle errors
 @app.errorhandler(500)
